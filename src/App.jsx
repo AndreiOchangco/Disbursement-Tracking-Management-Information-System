686b5995a171001011529d8919c264cf2fafee13
Andrei Ochangco <196190939+AndreiOchangco@users.noreply.github.com>
2026-04-19 20:30:26 +0800

    Added ionicons designer pack for easier integration and moved the App.css inside the css folder for organized project structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/AppRoute'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import './App.css'
+import './css/App.css'
 
 function App() {
   return (
